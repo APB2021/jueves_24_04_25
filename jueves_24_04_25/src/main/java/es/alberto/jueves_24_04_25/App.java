e6dfc6d67db296e4dc3fee1dbbcca081d5f6e8bb
alumno <alumno@dam203>
2025-04-24 09:50:27 +0200

    Jueves 24_04_25

---
+++ jueves_24_04_25/src/main/java/es/alberto/jueves_24_04_25/App.java
@@ -0,0 +1,10 @@
+package es.alberto.jueves_24_04_25;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
